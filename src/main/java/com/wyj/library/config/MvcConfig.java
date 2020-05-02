35f6a1226c219e4611580a3ea6b3fb132d08a6d1
onlywyj <wyj669666@outlook.com>
2020-05-02 22:10:49 +0800

    add:Interceptor

---
+++ src/main/java/com/wyj/library/config/MvcConfig.java
@@ -0,0 +1,53 @@
+package com.wyj.library.config;
+
+import com.wyj.library.utils.LoginHandlerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index.html").setViewName("main");
+    }
+
+    //注册连接器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/","","/login","/loginForm");
+    }
+
+    //放行静态资源
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+
+
+
+    //    @Bean
+//    public WebMvcConfigurer webMvcConfigurer(){
+//        WebMvcConfigurer configurer = new WebMvcConfigurer() {
+//            //注册连接器
+//            @Override
+//            public void addInterceptors(InterceptorRegistry registry) {
+//                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+//                        .excludePathPatterns("/loginForm","/404","/login");
+//            }
+//
+//            //放行静态资源
+//            @Override
+//            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//                registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//            }
+//
+//        };
+//        return configurer;
+//    }
+
+}
---
+++ src/main/java/com/wyj/library/utils/LoginHandlerInterceptor.java
@@ -0,0 +1,37 @@
+package com.wyj.library.utils;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 登录检查
+ */
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object user = request.getSession().getAttribute("loginUser");
+        if (user == null){
+            //未登录，返回登录页面
+            request.setAttribute("message","没有权限请先登录!");
+            request.getRequestDispatcher("loginForm").forward(request,response);
+            return false;
+        }else {
+            //已登录，执行请求
+            return true;
+
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
